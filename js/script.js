c43667929e9cf86d84bc7cb549d6c5a120bf7218
GIrigoin <guilleirigoin@hotmail.com>
2024-08-30 17:23:27 -0300

    Challenge completed

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ let word = "";
 let scrambledWord = "";
 let tries = 0;
 let mistakes = [];
+let currentInputIndex = 0;
+const MAX_TRIES = 5; //starts at 0
 
 //random y reset: random genera otra palabra y reset resetea el juego para la misma. Entonces random usa reset para inicializar el juego. reset deberia recibir la palabra y el scramble, sino al resetear el scramble seria distinto
 
@@ -24,7 +26,7 @@ const generateNewWord = async () => {
     const data = await response.json();
     word = data[0];
     scrambleWord();
-    console.log(word, scrambledWord);
+    // console.log(word, scrambledWord);
   } catch (error) {
     alert("Oops, something failed");
   }
@@ -33,12 +35,111 @@ const generateNewWord = async () => {
 const drawNewTry = () => {
   const inputsContainer =
     document.getElementsByClassName("inputs-container")[0];
-  word.forEach((letter, index) => {
-    console.log(letter);
+  inputsContainer.replaceChildren();
+  const letters = word.split("");
+
+  letters.forEach((letter, index) => {
+    const newInput = document.createElement("p");
+    newInput.className = "inputs";
+    if (index === 0) {
+      newInput.className = "inputs input-active";
+    }
+    newInput.innerText = "_";
+    newInput.id = index;
+    inputsContainer.appendChild(newInput);
   });
 };
 
-const resetGame = () => {};
+const inputListener = async (event) => {
+  const inputValidation = /^[a-zA-Z]$/;
 
-generateNewWord();
-drawNewTry();
+  if (inputValidation.test(event.key)) {
+    const currentInput = document.getElementById(currentInputIndex);
+    currentInput.innerText = event.key;
+
+    if (event.key.toUpperCase() !== word[currentInputIndex].toUpperCase()) {
+      mistakes.push(event.key);
+      const mistakesDisplay = document.getElementsByClassName("mistakes")[0];
+      mistakesDisplay.innerText = mistakes.join(", ");
+    }
+    currentInputIndex++;
+
+    if (currentInputIndex === word.length) {
+      if (mistakes.length === 0) {
+        await setTimeout(() => {
+          alert("🎉 Success!");
+          newGame();
+        }, 500);
+
+        return;
+      }
+
+      const triesDisplays = document.getElementsByClassName("try");
+      triesDisplays[tries].classList.toggle("try-wasted");
+      tries++;
+      if (tries === MAX_TRIES) {
+        await setTimeout(() => {
+          resetGame();
+          alert("Try again! 🚀🚀🚀");
+        }, 500);
+        return;
+      }
+
+      currentInputIndex = 0;
+      mistakes = [];
+      const triesLabel = document.getElementsByClassName("tries-label")[0];
+      const mistakesDisplay = document.getElementsByClassName("mistakes")[0];
+      await setTimeout(() => {
+        triesLabel.innerText = `Tries (${tries}/5): `;
+        mistakesDisplay.innerText = "";
+        drawNewTry();
+      }, 1000);
+    }
+    if (currentInputIndex > 0) {
+      currentInput.classList.toggle("input-active");
+      const nextInput = document.getElementById(currentInputIndex);
+      nextInput.classList.toggle("input-active");
+    }
+  }
+};
+
+const resetGame = () => {
+  tries = 0;
+  mistakes = [];
+  currentInputIndex = 0;
+  const triesLabels = document.getElementsByClassName("tries-label")[0];
+  triesLabels.innerText = "Tries (0/5): ";
+  const triesDisplays = document.getElementsByClassName("tries")[0];
+  triesDisplays.replaceChildren();
+  for (let index = 0; index < MAX_TRIES; index++) {
+    const element = document.createElement("div");
+    element.className = "try";
+    triesDisplays.appendChild(element);
+  }
+
+  const scrambled = document.getElementsByClassName("scrambled-word")[0];
+  scrambled.innerText = scrambledWord;
+
+  const mistakesDisplay = document.getElementsByClassName("mistakes")[0];
+  mistakesDisplay.innerText = "";
+
+  drawNewTry();
+};
+
+const newGame = async () => {
+  await generateNewWord();
+
+  resetGame();
+};
+
+// generateNewWord();
+// drawNewTry();
+newGame();
+
+const randomButton = document.getElementById("random-button");
+randomButton.addEventListener("click", newGame);
+
+const resetButton = document.getElementById("reset-button");
+resetButton.addEventListener("click", resetGame);
+
+document.addEventListener("keydown", inputListener);
